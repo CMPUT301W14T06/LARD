d7d7dfe7ee37d70ff2c8b66774fac2b392884b3b
elake <elake@ualberta.ca>
2014-02-14 18:10:48 -0700

    Main UI mocked up

--- gen/ca/ualberta/lard/R.java
+++ gen/ca/ualberta/lard/R.java
@@ -21,13 +21,21 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int home=0x7f020000;
+        public static final int ic_action_new=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_favourites=0x7f080004;
+        public static final int action_location=0x7f080005;
+        public static final int action_new=0x7f080002;
+        public static final int action_sort=0x7f080003;
+        public static final int listView1=0x7f080001;
+        public static final int textView1=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int list_item=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -35,7 +43,12 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int favourites=0x7f050005;
         public static final int hello_world=0x7f050002;
+        public static final int location=0x7f050006;
+        public static final int news=0x7f050003;
+        public static final int sort=0x7f050004;
+        public static final int threads=0x7f050007;
     }
     public static final class style {
         /** 
