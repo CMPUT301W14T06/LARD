baef7ef5340afb203b18fc479b66415d71cb3423
curopium <curnow@ualberta.ca>
2014-02-25 17:20:31 -0700

    Files changes to add NewCommentActivity

--- LARD/src/ca/ualberta/lard/MainActivity.java
+++ LARD/src/ca/ualberta/lard/MainActivity.java
@@ -1,8 +1,12 @@
 package ca.ualberta.lard;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
 public class MainActivity extends Activity {
@@ -11,6 +15,22 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Button button = (Button) findViewById(R.id.BadBadDeleteMe);
+        
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+            	
+                Intent intent = new Intent(MainActivity.this,
+                        NewCommentActivity.class);
+                startActivity(intent);
+
+            }
+            
+        });
+        
+        
     }
 
 
