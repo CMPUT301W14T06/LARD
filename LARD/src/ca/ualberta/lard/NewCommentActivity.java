78d4617f18baf326514057e6aa40e353ae4e01e0
curopium <curnow@ualberta.ca>
2014-02-25 17:02:41 -0700

    Created NewCommentActivity Skeleton

---
+++ LARD/src/ca/ualberta/lard/NewCommentActivity.java
@@ -0,0 +1,22 @@
+package ca.ualberta.lard;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class NewCommentActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_new_comment);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.new_comment, menu);
+		return true;
+	}
+
+}
