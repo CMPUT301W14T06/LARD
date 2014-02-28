9ecffae56a6f1b7d8125ff1abf70abbccd338341
Victoria Bobey <vbobey@ualberta.ca>
2014-02-27 19:45:12 -0700

    CommentActivity updated so list should be clickable, however will not display any comments since .children isnt implemented yet in Comment

--- LARD/src/ca/ualberta/lard/CommentActivity.java
+++ LARD/src/ca/ualberta/lard/CommentActivity.java
@@ -1,48 +1,80 @@
 /**
- * When called pass the id of the comment through intent.
+ * When called, the id of the top level comment is received via intent.
  */
 
 package ca.ualberta.lard;
 
-import ca.ualberta.lard.model.Comment;
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import ca.ualberta.lard.model.Comment;
 
 public class CommentActivity extends Activity {
-	//private int parentId;
+	private String parentId;
 	private ListView commentListView;
 	private Comment parent;
+	private ArrayList<Comment> commentList;
+	private ArrayList<String> commentStrList;
+	private ArrayAdapter<String> adapter;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_comment);
 	    
-	    // Configure the listview, make it clickable
+	    // Configure the list view
 	    commentListView = (ListView)findViewById(R.id.toplevel_and_children_list);
+	    commentListView.setOnItemClickListener(new OnItemClickListener() {
+	    	@Override
+	    	// Opens another CommentActivity when a comment on the list is clicked
+	    	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+	    		Intent intent = new Intent(getApplicationContext(),CommentActivity.class);
+	    		// Get the id of the comment that was clicked
+	    		String clickedCommentId = commentList.get(position).getId();
+	    		intent.putExtra("extra_parent_id", clickedCommentId);
+	    		startActivity(intent);		
+	    	}	    	
+		}); 
 	    
 	    // Get the id of the top level comment from intent
 	    Intent intent = getIntent();
-	    //parentId = (int) intent.getIntExtra("extra_parent_id", 0);
-	    parent = (Comment) intent.getSerializableExtra("extra_comment");
+	    parentId = (String)intent.getStringExtra("extra_parent_id");
+	    
+	    // TODO: Get the comment by passing the id to the controller
+	    // parent = ??
 	}
 	
 	@Override
 	protected void onResume() {
 		super.onResume();	
-		// Create list of comments to display based off parentId.
-		parent.getChildren();
-	    // Based on id, get list of children
-	    // Create a list with top level comment followed by all children
-	    // Display this list	
+		// TODO: Create list of comments to display based off parentId.
+		// commentList = parent.children();
+	    // commentList.add(0, parent);
+		
+		// Create a list of strings for the adapter
+		// TODO: Decide what part of comment to display. Currently only shows bodyText.
+		commentStrList = new ArrayList<String>();
+		for(Comment item: commentList) {
+			commentStrList.add(item.getBodyText());
+		}
+		
+		// Display parent comment and replies
+		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, commentStrList);
+		commentListView.setAdapter(adapter);
 	}
 
 	@Override
 	protected void onPause() {
+		// Nothing to be done onPause?
 	}
 	
 	@Override
@@ -64,8 +96,10 @@ public class CommentActivity extends Activity {
         	// TODO: Add comment to favourites
             return true;
         case R.id.action_reply:
-            // Reply to a comment
-        	// TODO: Open NewCommentActivity -> Pass it parent comment?
+            // Reply to a comment (open NewCommentActivity)
+    		Intent intent = new Intent(getApplicationContext(),NewCommentActivity.class);
+    		intent.putExtra("extra_parent_id", parentId);
+    		startActivity(intent);
             return true;
         case R.id.action_save:
             // Save a comment for later
@@ -75,7 +109,6 @@ public class CommentActivity extends Activity {
             return super.onOptionsItemSelected(item);
         }
     }
-
 }
 
 
---
+++ LARDTestTest/src/ca/ualberta/lard/test/CommentActivityTests.java
@@ -0,0 +1,14 @@
+package ca.ualberta.lard.test;
+
+import ca.ualberta.lard.CommentActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class CommentActivityTests extends ActivityInstrumentationTestCase2<CommentActivity> {
+
+	public CommentActivityTests() {
+		super(CommentActivity.class);
+	}
+	
+	
+
+}
