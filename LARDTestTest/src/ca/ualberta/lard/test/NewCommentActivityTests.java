01ffa2876fe08bde8b65ed7c3facb6fb61c9bc6c
DylanCassidy <the.dylan.cassidy@gmail.com>
2014-03-02 14:58:31 -0700

    Added JUnit test for NewCommentActivity

---
+++ LARDTestTest/src/ca/ualberta/lard/test/NewCommentActivityTests.java
@@ -0,0 +1,13 @@
+package ca.ualberta.lard.test;
+
+import ca.ualberta.lard.CommentActivity;
+import ca.ualberta.lard.NewCommentActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+public class NewCommentActivityTests extends ActivityInstrumentationTestCase2<NewCommentActivity> {
+	
+	public NewCommentActivityTests() {
+		super(NewCommentActivity.class);
+	}
+
+}
