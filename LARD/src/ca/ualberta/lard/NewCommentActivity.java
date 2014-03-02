02bcc18d89c70ee6066c7fb525910d894c7743e6
DylanCassidy <the.dylan.cassidy@gmail.com>
2014-03-02 14:51:47 -0700

    NewCommentActivity checks it has a parent id

--- LARD/src/ca/ualberta/lard/NewCommentActivity.java
+++ LARD/src/ca/ualberta/lard/NewCommentActivity.java
@@ -2,14 +2,21 @@ package ca.ualberta.lard;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 
 public class NewCommentActivity extends Activity {
+	int pid[];
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new_comment);
+		
+		// get the parent id out of the intent
+		// will be null if this is a top level comment
+		Intent intent = getIntent();
+	    pid = intent.getIntArrayExtra("parentID");
 	}
 
 	@Override
