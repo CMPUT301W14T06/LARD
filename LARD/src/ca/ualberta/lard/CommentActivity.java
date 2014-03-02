f9cd3aada49b6f4e0bb406eb192523e067bb9d26
Victoria Bobey <vbobey@ualberta.ca>
2014-03-02 13:57:56 -0700

    Worked on tests for CommentActivityTest

--- LARD/src/ca/ualberta/lard/CommentActivity.java
+++ LARD/src/ca/ualberta/lard/CommentActivity.java
@@ -17,6 +17,8 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import ca.ualberta.lard.model.Comment;
+import ca.ualberta.lard.model.CommentRequest;
+import ca.ualberta.lard.model.DataModel;
 
 public class CommentActivity extends Activity {
 	private String parentId;
@@ -26,10 +28,23 @@ public class CommentActivity extends Activity {
 	private ArrayList<String> commentStrList;
 	private ArrayAdapter<String> adapter;
 	
+	// For getting the parent id from the extra
+	public static final String EXTRA_PARENT_ID = "TEXT";
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_comment);
+	    
+	    // Get the id of the top level comment from intent
+	    Intent intent = getIntent();
+	    parentId = (String)intent.getStringExtra(EXTRA_PARENT_ID);
+	    
+	    // Get the comment by passing the id to the controller
+	    CommentRequest parentRequest = new CommentRequest(1);
+	    parentRequest.setId(parentId);
+	    parent = DataModel.retrieveComments(parentRequest).get(0);
 	    
 	    // Configure the list view
 	    commentListView = (ListView)findViewById(R.id.toplevel_and_children_list);
@@ -40,25 +55,23 @@ public class CommentActivity extends Activity {
 	    		Intent intent = new Intent(getApplicationContext(),CommentActivity.class);
 	    		// Get the id of the comment that was clicked
 	    		String clickedCommentId = commentList.get(position).getId();
-	    		intent.putExtra("extra_parent_id", clickedCommentId);
+	    		intent.putExtra(EXTRA_PARENT_ID, clickedCommentId);
 	    		startActivity(intent);		
 	    	}	    	
 		}); 
-	    
-	    // Get the id of the top level comment from intent
-	    Intent intent = getIntent();
-	    parentId = (String)intent.getStringExtra("extra_parent_id");
-	    
-	    // TODO: Get the comment by passing the id to the controller
-	    // parent = ??
 	}
 	
 	@Override
 	protected void onResume() {
 		super.onResume();	
 		// TODO: Create list of comments to display based off parentId.
-		// commentList = parent.children();
-	    // commentList.add(0, parent);
+		if (parent.children() == null) {
+			commentList = new ArrayList<Comment>();
+		}
+		else {
+			commentList = parent.children();
+		}
+	    commentList.add(0, parent);
 		
 		// Create a list of strings for the adapter
 		// TODO: Decide what part of comment to display. Currently only shows bodyText.
@@ -71,11 +84,6 @@ public class CommentActivity extends Activity {
 		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, commentStrList);
 		commentListView.setAdapter(adapter);
 	}
-
-	@Override
-	protected void onPause() {
-		// Nothing to be done onPause?
-	}
 	
 	@Override
 	// Display action bar
@@ -84,9 +92,9 @@ public class CommentActivity extends Activity {
 	    return super.onCreateOptionsMenu(menu);
 	}
 	
-    /**
-     *  Deals with on click for action bar
-     *  Nothing is implemented yet here
+    /** 
+     * The action bar has 3 options: favourite, reply, save.
+     * These options all correspond to the parent comment.
      */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -94,16 +102,18 @@ public class CommentActivity extends Activity {
         case R.id.action_fav:
             // Favourite a comment
         	// TODO: Add comment to favourites
+        	DataModel.saveLocal(parent, true);
             return true;
         case R.id.action_reply:
             // Reply to a comment (open NewCommentActivity)
-    		Intent intent = new Intent(getApplicationContext(),NewCommentActivity.class);
-    		intent.putExtra("extra_parent_id", parentId);
+    		Intent intent = new Intent(getApplicationContext(), NewCommentActivity.class);
+    		intent.putExtra(CommentActivity.EXTRA_PARENT_ID, parentId);
     		startActivity(intent);
             return true;
         case R.id.action_save:
             // Save a comment for later
         	// TODO: Save
+        	DataModel.saveLocal(parent, true);
             return true;
         default:
             return super.onOptionsItemSelected(item);
