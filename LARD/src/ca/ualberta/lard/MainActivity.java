8ef675d540f19d7454594c10a4e37d1bd91378ea
elake <elake@ualberta.ca>
2014-03-05 18:26:58 -0700

    Wraps long bodytext and authors

--- LARD/src/ca/ualberta/lard/MainActivity.java
+++ LARD/src/ca/ualberta/lard/MainActivity.java
@@ -44,6 +44,7 @@ allComments = new ArrayList<BarrenComment>();
 allComments.add(new BarrenComment("Test comment.", "Anonymous", false, 60, 9));
 allComments.add(new BarrenComment("Another comment!", "Sir Maynard", true, 1200, 0));
 allComments.add(new BarrenComment("Dogz suk catz4lyfe", "catlover66", false, 10, 9001));
+allComments.add(new BarrenComment("LOL MY NAME IS SO LONG AND SO IS THIS POST OMG I HOPE IT DOESNT MESS UP MY BALLER VIEW", "guywithalongnamethatsfartoolongforthisbox", false, 10, 9001));
 adapter = new CommentListBaseAdapter(this,
  allComments);
 commentList.setAdapter(adapter);
