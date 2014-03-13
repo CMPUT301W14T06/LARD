442f506128806422ad387f24ffa0753891c4cc92
DylanCassidy <the.dylan.cassidy@gmail.com>
2014-03-13 14:41:16 -0600

    fixed

--- LARD/src/ca/ualberta/lard/CommentActivity.java
+++ LARD/src/ca/ualberta/lard/CommentActivity.java
@@ -40,21 +40,27 @@ public class CommentActivity extends Activity {
 	// For debugging purposes
 	private static final String TAG = "Comment Activity";
 	
+	// For getting the parent id from the extra
+	public static final String EXTRA_PARENT_ID = "TEXT";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_comment);
         ActionBar actionBar = getActionBar();
-        actionBar.setHomeButtonEnabled(true);
+        actionBar.setHomeButtonEnabled(true);  
         
+        Log.d(TAG, "1. Start");
+	    
 	    // Get the id of the top level comment from intent
 	    Intent intent = getIntent();
-	    commentId = (String)intent.getStringExtra(NewCommentActivity.EXTRA_PARENT_ID);
+	    commentId = (String)intent.getStringExtra(EXTRA_PARENT_ID);
 	    
+	    Log.d(TAG, "2. Got id");
+
 	    // Get the comment by passing the id to the controller
 	    CommentRequest commentRequest = new CommentRequest(1);
 	    commentRequest.setId(commentId);
-	    
 	    // Check a comment was received 
 	    ArrayList<Comment> temp = DataModel.retrieveComments(commentRequest);
 	    if (temp == null) {
@@ -62,6 +68,8 @@ public class CommentActivity extends Activity {
 	    	finish();
 	    }
 	    comment = temp.get(0);
+	    
+	    Log.d(TAG, "3. Got comment. Id is "+comment.getId());
 	    
 	    // Configure the list view
 	    commentListView = (ListView)findViewById(R.id.toplevel_and_children_list);
@@ -72,7 +80,7 @@ public class CommentActivity extends Activity {
 	    		Intent intent = new Intent(getApplicationContext(),CommentActivity.class);
 	    		// Get the id of the comment that was clicked
 	    		String clickedCommentId = commentList.get(position).getId();
-	    		intent.putExtra(NewCommentActivity.EXTRA_PARENT_ID, clickedCommentId);
+	    		intent.putExtra(CommentActivity.EXTRA_PARENT_ID, clickedCommentId);
 	    		startActivity(intent);		
 	    	}	    	
 		});
@@ -83,18 +91,18 @@ public class CommentActivity extends Activity {
 	protected void onResume() {
 		super.onResume();	
 		
-		// Get the replies to display
+		Log.d(TAG, "4. Getting children.");
+		
 		if (comment.children() == null) {
 			commentList = new ArrayList<Comment>();
 		}
 		else {
 			commentList = comment.children();
 		}
-		
-		// Add the children to the front of the list
 	    commentList.add(0, comment);
 	    
-	    // Set up the adapter
+	    Log.d(TAG, "5. Got Children.");
+	
 	    adapter = new CommentListBaseAdapter(this, commentList);
 	    commentListView.setAdapter(adapter);
 	} 
@@ -116,7 +124,7 @@ public class CommentActivity extends Activity {
             return true;
         case R.id.action_reply:
     		Intent intent = new Intent(getApplicationContext(), NewCommentActivity.class);
-    		intent.putExtra(NewCommentActivity.EXTRA_PARENT_ID, commentId);
+    		intent.putExtra(CommentActivity.EXTRA_PARENT_ID, commentId);
     		startActivity(intent);
             return true;
         case R.id.action_save:
