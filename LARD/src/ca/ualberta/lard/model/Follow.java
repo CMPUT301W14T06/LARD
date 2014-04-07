a791d9f4c95b42c9e4af04f9bb1fc21bb532eb50
Troy Pavlek <ebonwumon@depotwarehouse.net>
2014-04-06 20:12:06 -0600

    Commented Follow Model

--- LARD/src/ca/ualberta/lard/model/Follow.java
+++ LARD/src/ca/ualberta/lard/model/Follow.java
@@ -6,21 +6,42 @@ import java.util.Set;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 
+/**
+ * The object that manages the users that we are following. Like favourites, it requires a SharedPreference
+ * in order to function.
+ * @author Troy Pavlek
+ *
+ */
 public class Follow {
+	// Name of the Shared Preference file
 	public static final String PREFS_NAME = "follows.pref";
+	// Name of the object in the Shared Preference containing the Author names.
 	private final String PREFS_OBJ = "followsList";
 	private SharedPreferences preferences;
 	private Set<String> followsList;
 	
+	/**
+	 * Instantiate a new Follow object (Constructor)
+	 * @param prefs SharedPreferences The preferences object containing the data we want.
+	 */
 	public Follow(SharedPreferences prefs) {
 		preferences = prefs;
 		followsList = preferences.getStringSet(PREFS_OBJ, new HashSet<String>());
 	}
 	
+	/**
+	 * Is the user following the user specified?
+	 * @param id String name of the author in question
+	 * @return boolean Are we following id?
+	 */
 	public boolean isFollowing(String id) {
 		return followsList.contains(id);
 	}
 	
+	/**
+	 * Follow an author
+	 * @param id String the author we wish to follow
+	 */
 	public void addFollow(String id) {
 		// We don't want to do anything if the comment is already a favourite
 		if (this.isFollowing(id)) {
@@ -38,6 +59,10 @@ public class Follow {
 		editor.commit();
 	}
 
+	/**
+	 * Gets the set of author names that we follow
+	 * @return Set<String> Followed author names
+	 */
 	public Set<String> getFollowed() {
 		return followsList;
 	}
