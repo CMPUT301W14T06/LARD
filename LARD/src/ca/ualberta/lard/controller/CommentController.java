78b6746bbb5d79d20428aea643851390dc860acb
Troy Pavlek <ebonwumon@depotwarehouse.net>
2014-04-06 21:00:33 -0600

    Documented CommentController

--- LARD/src/ca/ualberta/lard/controller/CommentController.java
+++ LARD/src/ca/ualberta/lard/controller/CommentController.java
@@ -23,67 +23,87 @@ import ca.ualberta.lard.model.GeoLocation;
  *
  */
 public class CommentController {
-	@SuppressWarnings("unused") // TODO: Remove
-	private Context context; // Needed for saveLocal
+	// Needed for a variety of tasks
+	private Context context;
+	// The buffer of loaded comments from the server
 	private ArrayList<Comment> buffer;
 	
 	/**
-	 * 
-	 * @param req
+	 * Initializes CommentController's buffer using a commentRequest
+	 * @param req CommentRequest the comments we want
 	 */
 	public void init(CommentRequest req) {
 		buffer = DataModel.retrieveComments(req);
 	}
+	
+	/**
+	 * Initialize with Context. This will work, even if the server is down (but has overhead of managing contexts)
+	 * @param req CommmentRequest the comments we want
+	 * @param context Context Application context
+	 */
 	public void init(CommentRequest req, Context context) {
 		buffer = DataModel.retrieveComments(req, context);
 	}
 	
+	/**
+	 * Constructor for when we only need to sort, not retrieve comments.
+	 */
 	public CommentController() {
-		// used when we just need to sort without getting comments
 	}
 	
 	/**
-	 * 
-	 * @param context
+	 * Constructor with context (Gets comments without specification)
+	 * @param context Context Application context
 	 */
 	public CommentController(Context context) {
-		CommentRequest req = new CommentRequest(10); //TODO pull this from configuration.
+		CommentRequest req = new CommentRequest(10);
 		req.setLocation(new GeoLocation(context));
 		this.context = context;
 		init(req, context);
 	}
 	
 	/**
-	 * 
-	 * @param req
+	 * Constructor with a specific request.
+	 * @param req CommentRequest the comments we want
+	 * @param context Context Application context
 	 */
 	public CommentController(CommentRequest req, Context context) {
 		this.context = context;
 		init(req, context);
 	}
 	
+	/**
+	 * Requests more comments.
+	 * @param req CommentRequest The comments we want
+	 * @return CommentController Itself, for chaining
+	 */
 	public CommentController request(CommentRequest req) {
 		init(req);
 		return this;
 	}
 	
 	/**
-	 * Creates a comment and saves it to disk
-	 * </p>
+	 * Creates a comment and saves it.
 	 * @param comment The comment object that is to be created.
+	 * @return boolean Did it succeed?
 	 */
 	public static boolean createComment(Comment comment) {
 		return DataModel.save(comment);
 	}
 	
+	/**
+	 * Updates a comment.
+	 * @param comment The updated version of the comment
+	 * @return boolean did it work?
+	 */
 	public static boolean update(Comment comment) {
 		return DataModel.update(comment);
 	}
 	
-	public boolean any() {
-		return buffer.size() > 0;
-	}
-	
+	/**
+	 * Are there any comments in the buffer?
+	 * @return boolean Are there comments in the buffer?
+	 */
 	public boolean isEmpty() {
 		return (buffer == null || buffer.size() == 0);
 	}
@@ -144,6 +164,10 @@ public class CommentController {
 		return comments;	
 	}
 	
+	/**
+	 * Gets the comments in the buffer
+	 * @return ArrayList<Comment> comments in the buffer
+	 */
 	public ArrayList<Comment> get() {
 		return buffer;
 	}
@@ -158,6 +182,10 @@ public class CommentController {
 		return buffer.get(0);
 	}
 	
+	/**
+	 * Favourites a comment (adds it to the favourites list, and saves the comment and its children to disk)
+	 * @param comment Comment we want to favourite
+	 */
 	public void favourite(Comment comment) {
 		Favourites favourites = this.getFavourites();
 		favourites.addFavourite(comment.getId());
@@ -174,6 +202,10 @@ public class CommentController {
 		return true;
 	}
 	
+	/**
+	 * Gets a favourites object, representing our favourites
+	 * @return Favourites Object with our favourites
+	 */
 	public Favourites getFavourites() {
 		SharedPreferences prefs = context.getSharedPreferences(Favourites.PREFS_NAME, Context.MODE_PRIVATE);
 		if (prefs == null) {
@@ -182,6 +214,10 @@ public class CommentController {
 		return new Favourites(prefs);
 	}
 	
+	/**
+	 * Gets a Follow object, representing the users that we follow
+	 * @return Follow the Users  that we follow
+	 */
 	public Follow getFollows() {
 		SharedPreferences prefs = context.getSharedPreferences(Follow.PREFS_NAME, Context.MODE_PRIVATE);
 		if (prefs == null) {
@@ -190,6 +226,10 @@ public class CommentController {
 		return new Follow(prefs);
 	}
 	
+	/**
+	 * Selects our favourite comments out of the buffer
+	 * @return ArrayList<Comment> The favourites in the buffer
+	 */
 	public ArrayList<Comment> getFavouriteComments() {
 		ArrayList<Comment> favoriteBuffer = new ArrayList<Comment>();
 		Set<String> favIDSet = this.getFavourites().getFavouritesList();
@@ -201,6 +241,10 @@ public class CommentController {
 		return favoriteBuffer;
 	}
 	
+	/**
+	 * Selects our followed Comments out of the buffer
+	 * @return ArrayList<Comment> our followed comments in the buffer
+	 */
 	public ArrayList<Comment> getFollowedComments() {
 		ArrayList<Comment> followedBuffer = new ArrayList<Comment>();
 		Set<String> followedIDSet = this.getFollows().getFollowed();
